539250035ed41c8ed45d4dac676015578c098b9b
Erick Trettel <ericktrettel@gmail.com>
2021-07-27 16:48:15 -0300

    Chapter 4

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -19,3 +19,14 @@ export const parameters = {
     },
   },
 }
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  // Storybook a11y addon configuration
+  a11y: {
+    // the target DOM element
+    element: '#root',
+    // sets the execution mode for the addon
+    manual: false,
+  },
+};
